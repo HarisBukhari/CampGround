be6fb5b7110bcb729347654b8b853ce66fd75b81
Syed Haris Bukhari <45495476+HarisBukhari@users.noreply.github.com>
2022-06-25 13:12:19 +0500

    Partial Crud Done

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express()
 const path = require("path")
 const mongoose = require('mongoose')
 const Campground = require('./models/campground')
+const methodOverride = require('method-override')
 
 
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended:true}))
+
 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'/views'))
@@ -27,19 +30,34 @@ app.get('/campgrounds',async (req,res)=>{
     res.render('campground/index',{campground})
 })
 
-app.get('/campgrounds/new',(req,res)=>{
-  res.render('campground/new')
-})
-
 app.post('/campgrounds',async (req,res)=>{
   const camp = new Campground(req.body)
   await camp.save()
   res.redirect(`/campgrounds/${camp._id}`)
 })
 
+app.put('/campgrounds/:id',async (req,res)=>{
+  const campground =await Campground.findByIdAndUpdate(req.params.id,{...req.body})
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/new',(req,res)=>{
+  res.render('campground/new')
+})
+
+app.get('/campgrounds/edit/:id',async (req,res)=>{
+  const campground =await Campground.findById(req.params.id)
+  res.render('campground/edit',{campground})
+})
+
 app.get('/campgrounds/:id',async (req,res)=>{
     const campground = await Campground.findById(req.params.id)
     res.render('campground/show',{campground})
+})
+
+app.delete('/campgrounds/:id',async (req,res)=>{
+  const campground = await Campground.findByIdAndRemove(req.params.id)
+  res.redirect(`/campgrounds/`)
 })
 
 
