612c282bfc86615da56f68310a4d57b95bd2e07c
Syed Haris Bukhari <45495476+HarisBukhari@users.noreply.github.com>
2022-06-27 10:52:25 +0500

    Major Error Handling

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
+const {campgroundSchema} =require('./utils/joischemas')
 
 app.use(express.json())
 app.engine('ejs',ejsMate)
@@ -17,7 +18,7 @@ app.use(express.urlencoded({extended:true}))
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'/views'))
 
-
+//Mongo Connection
 main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/campground',{
@@ -27,18 +28,32 @@ async function main() {
   console.log("Connection Open")
 }
 
+//Methods
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+      const msg = error.details.map(el => el.message).join(',')
+      throw new ExpressError(msg, 400)
+  } else {
+      next();
+  }
+}
+
+
+
+//Routes
 app.get('/campgrounds',catchAsync(async (req,res)=>{
     const campgrounds = await Campground.find({})
     res.render('campground/index',{campgrounds})
 }))
 
-app.post('/campgrounds',catchAsync(async (req,res)=>{
+app.post('/campgrounds',validateCampground,catchAsync(async (req,res)=>{
   const campground = new Campground(req.body.campground)
   await campground.save()
   res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-app.put('/campgrounds/:id',catchAsync(async (req,res)=>{
+app.put('/campgrounds/:id',validateCampground,catchAsync(async (req,res)=>{
   const campground =await Campground.findByIdAndUpdate(req.params.id,{...req.body.campground})
   res.redirect(`/campgrounds/${campground._id}`)
 }))
@@ -62,11 +77,12 @@ app.delete('/campgrounds/:id',catchAsync(async (req,res)=>{
   res.redirect(`/campgrounds/`)
 }))
 
-
 app.get('*',(req,res)=>{
     res.render('home')
 })
 
+
+//Middlewares
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404))
 })
@@ -77,6 +93,9 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err })
 })
 
+
+
+//Server
 app.listen(3000,(req,res)=>{
     console.log('Server is running port 3000h')
 })
