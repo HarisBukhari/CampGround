3e957728d7b536de74fc8ddf6315b0789b294271
Syed Haris Bukhari <45495476+HarisBukhari@users.noreply.github.com>
2022-07-02 17:59:50 +0500

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,35 +4,39 @@ const Campground = require('../models/campground')
 const catchAsync = require('../utils/catchAsync')
 const {isLoggedIn,validateCampground,isAuthor} = require('../middleware')
 
-
 router.get('/',catchAsync(async(req,res)=>{
     const campgrounds = await Campground.find({})
     res.render('campground/index',{campgrounds})
 }))
 
+router.get('/new',isLoggedIn,(req,res)=>{
+  res.render('campground/new')
+})
+
 router.post('/',isLoggedIn,validateCampground,catchAsync(async(req,res)=>{
   // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
   const campground = new Campground(req.body.campground)
+  campground.author = req.user._id
   await campground.save()
   req.flash('success', 'Successfully made a new Campground!')
   res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.put('/:id',isAuthor,isLoggedIn,validateCampground,catchAsync(async(req,res)=>{
+router.get('/:id',catchAsync(async(req,res)=>{
   //populate to access inner object and populate {} to access inner INNER object
   const campground = await Campground.findById(req.params.id).populate({
     path: 'reviews',
     populate: {
         path: 'author'
     }
-    }).populate('author');
-  req.flash('success', 'Successfully Updated Campground!')
-  res.redirect(`/campgrounds/${campground._id}`)
+    }).populate('author')
+  if (!campground) {
+    req.flash('error', 'Cannot find that campground!')
+    return res.redirect('/campgrounds')
+}
+  req.session.returnTo = req.originalUrl
+  res.render('campground/show',{campground})
 }))
-
-router.get('/new',isLoggedIn,(req,res)=>{
-  res.render('campground/new')
-})
 
 router.get('/:id/edit',isAuthor,isLoggedIn,catchAsync(async(req,res)=>{
   const campground =await Campground.findById(req.params.id)
@@ -43,14 +47,10 @@ router.get('/:id/edit',isAuthor,isLoggedIn,catchAsync(async(req,res)=>{
   res.render('campground/edit',{campground})
 }))
 
-router.get('/:id',catchAsync(async(req,res)=>{
-    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author')
-    if (!campground) {
-      req.flash('error', 'Cannot find that campground!')
-      return res.redirect('/campgrounds')
-  }
-    req.session.returnTo = req.originalUrl
-    res.render('campground/show',{campground})
+router.put('/:id',isAuthor,isLoggedIn,validateCampground,catchAsync(async(req,res)=>{
+  const campground =await Campground.findByIdAndUpdate(req.params.id,{...req.body.campground})
+  req.flash('success', 'Successfully Updated Campground!')
+  res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.delete('/:id',isAuthor,isLoggedIn,catchAsync(async(req,res)=>{
