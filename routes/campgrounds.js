0d646605b4016275fa22cdcb24d237304422a370
Syed Haris Bukhari <45495476+HarisBukhari@users.noreply.github.com>
2022-07-03 13:03:18 +0500

    Add-on

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,12 +3,19 @@ const router = express.Router()
 const catchAsync = require('../utils/catchAsync')
 const {isLoggedIn,validateCampground,isAuthor} = require('../middleware')
 const campgrounds = require('../controllers/campgrounds')
+const multer = require('multer')
+// const { storage } = require('../cloudinary')
+const upload = multer({ dest:'uploads/' })
 
 router.get('/',catchAsync(campgrounds.index))
 
 router.get('/new',isLoggedIn,campgrounds.newForm)
 
-router.post('/',isLoggedIn,validateCampground,catchAsync(campgrounds.createCamp))
+// router.post('/',isLoggedIn,validateCampground,catchAsync(campgrounds.createCamp))
+router.post('/',upload.array('image'),(req,res)=>{
+    console.log(req.body,req.files)
+    res.send("Done")
+})
 
 router.get('/:id',catchAsync(campgrounds.show))
 
